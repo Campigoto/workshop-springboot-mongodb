70804329e1fccefed3dc46a1a95255d42bf9050d
Campigoto <campigoto@hotmail.com>
2020-04-19 18:30:48 -0300

    Inserção de usuário com POST

--- src/main/java/com/campigoto/workshopmongo/services/UserService.java
+++ src/main/java/com/campigoto/workshopmongo/services/UserService.java
@@ -7,26 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.campigoto.workshopmongo.domain.User;
+import com.campigoto.workshopmongo.dto.UserDTO;
 import com.campigoto.workshopmongo.repository.UserRepository;
 import com.campigoto.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-		
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	
-	public User findById(String id) { 
-		  Optional<User> obj = repo.findById(id); 
-		  return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
-	 
-				
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
